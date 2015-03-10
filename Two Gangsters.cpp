a54598c622bcb32813b45a46160d0d1328ba3f88
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-03-09 21:26:11 -0400

    Create Two Gangsters.cpp

---
+++ Two Gangsters.cpp
@@ -0,0 +1,19 @@
+/*
+The answer basically lies in sybtracting one from each person's total available shots and then printing it in the opposite order
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a, b;
+
+	//Take two integers from the input
+	cin >> a >> b;
+
+	//Print the number that each player misses
+	cout << b-1 << " ";
+	cout << a-1;
+	return 0;
+}
