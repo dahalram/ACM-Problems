a739db045cff2d67a3613e9073f096d9acbc2fca
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-03-10 00:40:52 -0400

    Update Interesting Numbers.cpp

--- Interesting Numbers.cpp
+++ Interesting Numbers.cpp
@@ -1,3 +1,10 @@
+//Problem number 1385
+
+//The following implementation is correct. However the timus judge will give time limit exceeded error on test #4.
+//I will try to figure out time complexity and post the solution later.
+
+//The hack-y way of solving this problem is below.
+
 #include <iostream>
 #include <cmath>
 
@@ -9,41 +16,37 @@ int main(){
 
 	int first, second;
 	int counter = 0;
-	//int i = pow(10, 2*n -1) + 1;
 
 	for (int i = pow(10, 2*n -1) + 1; i < pow(10, 2*n); i++){
 		first = i /((pow(10, n)));
 		int b = pow(10,n);
 
 		second = i % b;
-		//cout << second <<endl;
-		//cout << i << endl;
 
 		if (((i % first) == 0) && (second != 0) &&(i % second) == 0){
 			counter++;
-			//cout << i << endl;
-			//cout <<first <<" " <<second<<endl;;
 		}
-
 	}
 	cout << counter;
 	return 0;
 }
 
+//The hack-y way of solving this problem is here.
 /*
 #include <cstdio>
 
 using namespace std;
 
 int main(){
-    int N;
-    scanf("%d",&N);
+    int n;
+    cin >>n;
     
-    if(N==1) printf("14");
-    else if(N==2) printf("155");
+    if(n==1) cout <<"14";
+    else if(n==2) cout <<"155";
     else{
-        printf("1575");
-        for(int i = 3;i<N;++i) putchar('0');
+        cout<<"1575";
+        for(int i = 3; i < n; ++i) 
+        	putchar('0');
     }
     
     return 0;
