7c255e04de1bfc618b175ebb16f35bef74cc269f
Ram Hari Dahal <dahalram@users.noreply.github.com>
2015-03-17 22:58:13 -0400

    Create Penguins.py

---
+++ Penguins.py
@@ -0,0 +1,20 @@
+
+
+a=raw_input()
+a=int(a)
+x={"Emperor Penguin":0, "Macaroni Penguin":0, "Little Penguin":0}
+
+for i in xrange(a):
+  m=raw_input()
+  x[m]+=1
+
+a=x["Emperor Penguin"]
+b=x["Macaroni Penguin"]
+c=x["Little Penguin"]
+
+if a>b and a>c:
+  print "Emperor Penguin"
+if b>a and b>c:
+  print "Macaroni Penguin"
+if c>a and c>b:
+  print "Little Penguin"
